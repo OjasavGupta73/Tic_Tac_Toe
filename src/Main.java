9bed594bd27dabb569ba9c2e169bfb0735cdb4f1
OjasavGupta73 <ojasavgupta73@gmail.com>
2024-10-10 02:33:34 +0530

    First Commit : Board overlapping solved

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Board game = new Board();
+        Board game = new Board();//
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter Player 1:");
