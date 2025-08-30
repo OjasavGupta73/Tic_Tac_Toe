dd8f6b60cdd667bb009010dc2e75d87dce79257e
OjasavGupta73 <ojasavgupta73@gmail.com>
2025-08-31 02:43:30 +0530

    Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Ultimate Tic Tac Toe");
         Board game = new Board();//
         Scanner scanner = new Scanner(System.in);
 
